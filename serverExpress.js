c75d75087f2ff620efdaa1b45acfacb066244096
Thomas Tiscornia <thomasezequielt@gmail.com>
2022-10-12 20:33:59 -0300

    desafio3

---
+++ serverExpress.js
@@ -0,0 +1,43 @@
+const Contenedor = require("./Contenedor");
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const contenedor = new Contenedor("productos.json");
+
+app.get('/', (req, res) => {
+    res.send('Hi Express Server!')
+});
+
+app.get('/productos', async (req, res) => {
+    const allProducts = await contenedor.getAll();
+    res.json(allProducts);
+});
+
+app.get('/productoRandom', async (req, res) => {
+    const allProducts = await contenedor.getAll();
+    const maxId = allProducts.length;
+
+    const randomNumber = generateRandomNumber(1, maxId);
+    const randomProduct = await contenedor.getById(randomNumber);
+
+    res.json(randomProduct);
+
+})
+
+app.get('/albums', (req, res) => {
+    res.send('Hi, you are at Albums')
+});
+
+app.get('/object', (req, res) => {
+    res.json({title: 'Comfort y música para volar', artist: 'Álbum musical de Soda Stereo'})
+});
+
+const generateRandomNumber = (min, max) => {
+    return Math.floor((Math.random() * (max+1 -min)) +min);
+}
+
+const server = app.listen(PORT, () => {
+    console.log(`>>>> Server started at http://localhost:${PORT}`)
+})
+
+server.on('error', (error) => console.log(error));
