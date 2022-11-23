e337bc075f471ee54f3d06c11417702ad53be031
Thomas Tiscornia <thomasezequielt@gmail.com>
2022-11-23 20:34:24 -0300

    entrega-9

--- src/configSQLite.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    client: "better-sqlite3",
-    connection: {
-      filename: "./DB/mydb.sqlite"
-    },
-    useNullAsDefault: true,
-  };
-  
--- src/db.js
+++
@@ -1,5 +0,0 @@
-import { config } from './config.js'; //MySql
-//import { config } from './configSQLite.js'; //SQLite
-import _knex from 'knex';
-//console.log(config); //To verify .env file is ok
-export const knex = _knex(config)
