a22dab886fc8f50e4ad4b70ae55f5b8bce79f6ff
Thomas Tiscornia <thomasezequielt@gmail.com>
2022-12-14 21:56:01 -0300

    Entrega-12

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { ProductoDao } from "../dao/ProductoDao.js";
 import { authMiddleware } from "../middlewares/Auth.js";
-import { ProductMocker } from '../mocks/productMocker.js'
 const router = express.Router();
 const productoDao = new ProductoDao();
 
@@ -26,14 +25,6 @@ router.get('/:id', async(req, res) => {
         ? res.status(200).json(product)
         : res.status(400).json({"error": "product not found"})
     
-})
-
-// GET api/productos/test/random
-
-router.get('/test/random', async(req,res) => {
-    const productMocker = new ProductMocker(5);
-    const products = productMocker.generateRandomProducts();
-    res.status(200).json(products);
 })
 
 
